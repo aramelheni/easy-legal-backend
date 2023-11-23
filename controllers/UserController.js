e49e19ba1cfae37ac941a9312b32cbca52a82aef
zeineb <zeineb.bendhiaf@medtech.tn>
2023-11-23 10:52:37 +0100

    added chats path

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,4 +2,75 @@ async function getUser(request, response) {
     response.status(200).json({ users: users });
 }
 
-module.exports = {getUser};
+
+async function getChat(request, response) {
+    const id1 = parseInt(request.params.id1)
+    const id2 = parseInt(request.params.id2)
+    const chats = [
+        {
+            ids:[0, 1],
+            messages: [
+                {
+                    senderId: 0,
+                    content: "ahla ya nahla",
+                    date: new Date()
+                },
+                {
+                    senderId: 1,
+                    content: "chbi el kattouss manbouz",
+                    date: new Date()
+                }
+            ]
+        }, {
+            ids:[0, 2],
+
+            messages: [
+                
+                {
+                    senderId: 2,
+                    content: "ahla ya nahla",
+                    date: new Date()
+                },
+                {
+                    senderId: 0,
+                    content: "chbi el kattouss manbouz",
+                    date: new Date()
+                }
+            ]
+        }, {
+            ids:[0, 3],
+
+            messages: [
+                {
+                    senderId: 0,
+                    content: "ahla ya nahla",
+                    date: new Date()
+                },
+                {
+                    senderId: 0,
+                    content: "chbi el kattouss manbouz",
+                    date: new Date()
+                },
+                {
+                    senderId: 3,
+                    content: "Khra nekrah web",
+                    date: new Date()
+                }
+            ]
+        }
+    ]
+
+    let result = null;
+
+    chats.forEach(chat=>{
+        console.log(id1, id2);
+        if(chat.ids[0]===id1 && chat.ids[1]===id2){
+            result=chat;
+        }
+    });
+    response.status(200).json(result)
+
+    
+}
+
+module.exports = { getUser, getChat };
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userRouter = express.Router(); //Create a Router instance
-const {getUser} = require("../controllers/UserController.js");
+const {getUser,getChat} = require("../controllers/UserController.js");
 
 const users = [
     {
@@ -15,5 +15,6 @@ const users = [
 ]
 
 userRouter.get("/users", getUser);
+userRouter.get("/users/chat/:id1/:id2",getChat)
 
 module.exports = userRouter;
